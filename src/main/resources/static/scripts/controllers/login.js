7c51b98b2c4fae83efca13195ab49e80c7478c6e
Konrad Ulman <veradopl@gmail.com>
2020-01-10 20:20:55 +0100

    login page

--- src/main/resources/static/scripts/controllers/login.js
+++ src/main/resources/static/scripts/controllers/login.js
@@ -2,11 +2,22 @@
 
 angular.module('petPatrolApp')
     .controller('LoginCtrl', ['$scope', '$rootScope', '$http', '$window', '$timeout', function ($scope, $rootScope, $http, $window, $timeout) {
+        $scope.isFailed = false;
+
         $scope.checkCredentials = function () {
             console.log('dsfsdf');
             console.log($scope.email, $scope.password);
             if($scope.email === 'ratownik@petpatrol.com' && $scope.password === 'ratownik123') {
                 window.location.href = "dashboard.html";
+            } else {
+                window.location.search = "error";
+            }
+
+        };
+
+        $scope.init = function() {
+            if(window.location.search.includes('error')) {
+                $scope.isFailed = true;
             }
         };
     }]);
